e270867082a3de4d8e9ce137cd4856c64f3faca7
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 16:25:08 -0300

    Nível 1

--- Online-Judges/URI/SQL/2607.sql
+++ Online-Judges/URI/SQL/2607.sql
@@ -1,5 +1,5 @@
 -- Nome: 2607 - Cidades em Ordem Alfabética
 -- Nível: 1
--- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2604
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2607
 
 select city from providers order by city asc;
