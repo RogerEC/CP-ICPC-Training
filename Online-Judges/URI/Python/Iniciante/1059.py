c7f744640608c69d68520eb741c2c447d91ae90b
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 12:06:46 -0300

    Nível 1 - Laços de Repetição

---
+++ Online-Judges/URI/Python/Iniciante/1059.py
@@ -0,0 +1,7 @@
+# Nome: 1059 - Números Pares
+# Nível: 1
+# Assunto: Laços de Repetição
+# URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1059
+
+for x in range(2, 101, 2):
+    print(x)
