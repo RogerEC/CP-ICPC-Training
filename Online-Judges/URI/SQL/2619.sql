27f4a2c37b4d34b3ab55476351758babda9fd019
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:41:14 -0300

    Nível 2

--- Online-Judges/URI/SQL/2619.sql
+++ Online-Judges/URI/SQL/2619.sql
@@ -1,1 +1,5 @@
+-- Nome: 2619 - Super Luxo
+-- Nível: 2
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2619
+
 select p.name, f.name, p.price from products p inner join categories c on p.id_categories = c.id inner join providers f on p.id_providers = f.id where p.price > 1000 and c.name = 'Super Luxury'
