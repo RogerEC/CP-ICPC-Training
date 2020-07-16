2e84c48e4d2e91677d2555c7f6ba32c4fb9d1a34
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 12:05:33 -0300

    Nível 1 - Laços de Repetição

---
+++ Online-Judges/URI/cpp/Iniciante/1059.cpp
@@ -0,0 +1,16 @@
+// Nome: 1059 - Números Pares
+// Nível: 1
+// Assunto: Laços de Repetição
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1059
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=2; i<=100; i+=2){
+        cout<<i<<endl;
+    }
+    return 0;
+}
