a1fcf83da85983669571e4ed4ffb4ea390d4c077
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:42:10 -0300

    Nível 3

--- Online-Judges/URI/SQL/2621.sql
+++ Online-Judges/URI/SQL/2621.sql
@@ -1,1 +1,5 @@
+-- Nome: 2621 - Quantidades entre 10 e 20
+-- Nível: 1
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2621
+
 select p.name from products p join providers f on p.id_providers=f.id where f.name like 'P%' and p.amount between 10 and 20
