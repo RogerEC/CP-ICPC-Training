0452a8db978f9face7c379f1c29d0640e771bb20
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 17:47:33 -0300

    Nível 6

--- Online-Judges/URI/SQL/2609.sql
+++ Online-Judges/URI/SQL/2609.sql
@@ -1,1 +1,5 @@
+-- Nome: 2609 - Produtos por Categoria
+-- Nível: 6
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2609
+
 select c.name, SUM(p.amount) as sum from products p join categories c on p.id_categories=c.id group by c.name
