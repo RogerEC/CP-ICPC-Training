2bba1550a0d267b395e591e183f378d025dbc55e
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:40:19 -0300

    Nível 3

--- Online-Judges/URI/SQL/2618.sql
+++ Online-Judges/URI/SQL/2618.sql
@@ -1,1 +1,5 @@
+-- Nome: 2618 - Produtos Importados
+-- Nível: 1
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2618
+
 select p.name, f.name, c.name from products p join providers f on p.id_providers=f.id join categories c on c.id=p.id_categories where f.name='Sansul SA' and c.name='Imported'
