05010f2405a1e699a84f374e3decb4716ef45d68
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:44:26 -0300

    Nível 3

--- Online-Judges/URI/SQL/2620.sql
+++ Online-Judges/URI/SQL/2620.sql
@@ -1,5 +1,6 @@
--- Nome: 2617 - Pedidos no primeiro semestre
+-- Nome: 2620 - Pedidos no primeiro semestre
 -- Nível: 3
--- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2617
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2620
 
-select c.name, o.id from customers c join orders o on o.id_customers=c.id where o.orders_date between '2016/01/01' and '2016/06/30'
+select c.name, o.id from customers c join orders o on o.id_customers=c.id
+where o.orders_date between '2016/01/01' and '2016/06/30'
