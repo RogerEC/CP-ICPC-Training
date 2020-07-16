03b7da4934fa0aec76e56bbe5715a34f645c64e7
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 16:06:48 -0300

    Nível 2

---
+++ Online-Judges/URI/SQL/2604.sql
@@ -0,0 +1,5 @@
+-- Problema: 2604 - Menores que 10 ou Maiores que 100
+-- Nível: 2
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2604
+
+SELECT id, name FROM products WHERE( price<10 OR price>100)
