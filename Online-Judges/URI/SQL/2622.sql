56926f74accea01247afd262d0bf9b8471cd5a51
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:43:37 -0300

    Nível 1

--- Online-Judges/URI/SQL/2622.sql
+++ Online-Judges/URI/SQL/2622.sql
@@ -1,1 +1,5 @@
+-- Nome: 2622 - Fornecedor Ajax SA
+-- Nível: 1
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2622
+
 select c.name from customers c join legal_person lp on lp.id_customers=c.id 
