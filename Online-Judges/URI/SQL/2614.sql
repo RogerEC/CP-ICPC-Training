bf1228fd2b03e636d1c690b3bc4cd0525172e82f
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 18:18:13 -0300

    Nível 5

--- Online-Judges/URI/SQL/2614.sql
+++ Online-Judges/URI/SQL/2614.sql
@@ -1,1 +1,5 @@
+-- Nome: 2614 - Locações de Setembro
+-- Nível: 5
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2614
+
 select c.name, r.rentals_date from customers c join rentals r on r.id_customers=c.id where r.rentals_date between '2016/09/01' and '2016/09/30'
