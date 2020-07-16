84c38eb49438b4f4e52e442e3f6582b00c41e62a
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:39:06 -0300

    Nível 1

--- Online-Judges/URI/SQL/2617.sql
+++ Online-Judges/URI/SQL/2617.sql
@@ -1,1 +1,5 @@
+-- Nome: 2617 - Fornecedor Ajax SA
+-- Nível: 1
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2617
+
 select p.name, f.name from products p join providers f on p.id_providers=f.id where f.name='Ajax SA'
