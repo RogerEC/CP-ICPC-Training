ecbe3bf1dcbf5cd403ec68b930caef021fdbd87e
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 18:17:32 -0300

    Nível 2

--- Online-Judges/URI/SQL/2613.sql
+++ Online-Judges/URI/SQL/2613.sql
@@ -1,1 +1,5 @@
+-- Nome: 2613 - Filmes em Promoção
+-- Nível: 2
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2613
+
 select m.id, m.name from movies m join prices p on m.id_prices=p.id where p.value < 2
