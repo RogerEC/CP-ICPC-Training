1e778842878ff53c9f44a3d4857e7c7edb128a9f
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 19:37:36 -0300

    Nível 5

--- Online-Judges/URI/SQL/2616.sql
+++ Online-Judges/URI/SQL/2616.sql
@@ -1,1 +1,5 @@
+-- Nome: 2616 - Nenhuma Locação
+-- Nível: 5
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2616
+
 select c.id, c.name from customers c where not exists(select * from locations l where l.id_customers=c.id)
