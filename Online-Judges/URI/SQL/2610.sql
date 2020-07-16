a87693ff5fbc54168eb8a67c0a9e157d29ab955a
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 17:46:39 -0300

    Nível 3

--- Online-Judges/URI/SQL/2610.sql
+++ Online-Judges/URI/SQL/2610.sql
@@ -1,1 +1,5 @@
+-- Nome: 2610 - Valor médio dos produtos
+-- Nível: 3
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2610
+
 select cast(avg(price) as numeric (10,2)) from products
