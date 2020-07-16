044eeec5b4bc65609fdfe28937da7643f77fe089
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-16 18:16:16 -0300

    Nível 4

--- Online-Judges/URI/SQL/2611.sql
+++ Online-Judges/URI/SQL/2611.sql
@@ -1,1 +1,5 @@
+-- Nome: 2611 - Filmes de Ação
+-- Nível: 4
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2611
+
 select m.id, m.name from movies m join genres g on m.id_genres=g.id where g.description='Action'
