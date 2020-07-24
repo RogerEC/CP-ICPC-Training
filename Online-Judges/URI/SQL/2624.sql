20c42a6a915c3f33214d80ee9da760c2d19aa0f5
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-23 22:56:59 -0300

    Nível 3

---
+++ Online-Judges/URI/SQL/2624.sql
@@ -0,0 +1,5 @@
+-- Nome: 2624 - Quantidades de Cidades por Clientes
+-- Nível: 3
+-- URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2624
+
+select Count(distinct(city)) from customers
